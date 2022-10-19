fdf216b789ff9f7657dacd11dd4f9dfca668ac63
makerdreick <91573840+makerdreick@users.noreply.github.com>
2022-10-18 22:26:34 -0400

    program que gestra los nummero y materias en temp cokkies

---
+++ src/main/java/Pra_emegentes/ContadorServlet.java
@@ -0,0 +1,44 @@
+package Pra_emegentes;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ContadorServlet", urlPatterns = {"/ContadorServlet"})
+public class ContadorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int contador = 0;
+        Cookie[] cukis = request.getCookies();
+        if (cukis != null) {
+            for (Cookie C : cukis) {
+                if (C.getName().equals("Visitas")) {
+                    contador = Integer.parseInt(C.getValue());
+                }
+            }
+        }
+        contador++;
+        Cookie c = new Cookie("visitas", Integer.toString(contador));
+        // establece el tiempo de la  cookie en segundos
+
+        c.setMaxAge(30);
+        response.addCookie(c);
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.print("Visitante No:" + contador);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>cont cokkie</title>
+    </head>
+    <body>
+        <h1>Contador  de Visitas</h1>
+        <a href="ContadorServlet"  >  Mostrar  el contador de  vistias   personalizado.</a>
+    </body>
+</html>
